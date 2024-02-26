347cd21e80802345d2204ebbdb6f05376cd3c999
Jonah Hirsch <crazydog115@gmail.com>
2024-02-25 18:10:58 -0800

    Update SessionInfoAuthenticator to not imply it's specific to handshakes

--- src/main/kotlin/com/boltfortesla/teslafleetsdk/handshake/SessionInfoAuthenticator.kt
+++ src/main/kotlin/com/boltfortesla/teslafleetsdk/handshake/SessionInfoAuthenticator.kt
@@ -1,20 +1,20 @@
 package com.boltfortesla.teslafleetsdk.handshake
 
-/** Authentications the SessionInfo from a handshake. */
+/** Authentications the SessionInfo from a command response. */
 internal interface SessionInfoAuthenticator {
   /**
    * Verifies that the [sessionInfo] provided by a Vehicle is valid.
    *
    * @param sharedSecret the [sharedSecret] to use as the HMAC key
    * @param vin the VIN of the vehicle that sent the response
-   * @param handshakeUuid the UUID of the handshake request
+   * @param requestUuid the UUID of the request
    * @param sessionInfo the raw Session Info from the response
    * @param sessionInfoTag the raw Session Info Tag from the response
    */
   fun authenticate(
     sharedSecret: ByteArray,
     vin: String,
-    handshakeUuid: ByteArray,
+    requestUuid: ByteArray,
     sessionInfo: ByteArray,
     sessionInfoTag: ByteArray
   )
