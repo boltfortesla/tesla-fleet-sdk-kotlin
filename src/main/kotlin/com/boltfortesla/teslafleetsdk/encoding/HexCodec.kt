1fb6a8365a43665436a7bacc328375a4be1da60b
Jonah Hirsch <jonahwh@gmail.com>
2024-02-25 20:36:07 -0800

    Replace hexcodec implementations with bouncycastle calls (#9)

--- src/main/kotlin/com/boltfortesla/teslafleetsdk/encoding/HexCodec.kt
+++ src/main/kotlin/com/boltfortesla/teslafleetsdk/encoding/HexCodec.kt
@@ -1,14 +1,12 @@
 package com.boltfortesla.teslafleetsdk.encoding
 
+import org.bouncycastle.util.encoders.Hex
+
 /** Encodes and decodes hexadecimal values to/from [ByteArray] objects. */
 object HexCodec {
   /** Decodes a hexadecimal string into a [ByteArray]. */
-  fun String.decodeHex(): ByteArray {
-    check(length % 2 == 0) { "Must have an even length" }
-
-    return chunked(2).map { it.toInt(16).toByte() }.toByteArray()
-  }
+  fun String.decodeHex(): ByteArray = Hex.decode(this)
 
   /** Encodes a [ByteArray] into a hexadecimal string representing its content. */
-  fun ByteArray.toHexString(): String = this.joinToString("") { "%02x".format(it) }
+  fun ByteArray.toHexString(): String = Hex.toHexString(this)
 }
